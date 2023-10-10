b44da728ae5b405fe34fbc784c108e8a15c00e0a
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-10-11 03:30:50 +0900

    #1 chore: set Project settings

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "settings":{
+        "react":{
+            "version":"detect"
+        }
+    },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:storybook/recommended"
+    ],
+    "overrides": [
+        {
+           /* "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }*/
+            "files":["**/*.stories.*"],
+            "rules":{
+                "import/no-annonymous-default-export":"off"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope":"off",
+        "react/no-unescaped-entities":"off"
+    }
+}
