f6070fc4ac197f1a67ba57a959bef2b5abc69c11
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-11-05 03:31:43 +0900

    style: multi step btn

--- src/components/Signup/pageBtn.tsx
+++ src/components/Signup/pageBtn.tsx
@@ -1,4 +1,5 @@
 import { useCurrentStep, useSignupFormActions, useFormValue } from 'store';
+import S from './styles';
 
 const PageBtn = () => {
   const currentStep = useCurrentStep();
@@ -35,14 +36,3 @@ const PageBtn = () => {
 };
 
 export default PageBtn;
-
-const BtnContainer = 'w-full flex flex-row justify-between';
-
-const BtnWrapper = `cursor-pointer px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-400 to-pink-600  text-white`;
-const BtnFill = (color: string) =>
-  `text-black block font-semibold bg-${color} rounded-full px-5 py-2 border-none disabled:bg-zinc-200 disabled:text-zinc-600`;
-const S = {
-  BtnContainer,
-  BtnWrapper,
-  BtnFill,
-};
--- src/components/Signup/styles.tsx
+++ src/components/Signup/styles.tsx
@@ -1,8 +1,16 @@
 const SignupContainer =
   'w-2/5 h-4/5 p-4 rounded-xl bg-white  shadow-lg shadow-zinc-300/50 flex flex-col items-center justify-between';
+const BtnContainer = 'w-full flex flex-row justify-between';
+
+const BtnWrapper = `cursor-pointer px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-400 to-pink-600  text-white`;
+const BtnFill = (color: string) =>
+  `text-black block font-semibold bg-${color} rounded-full px-5 py-2 border-none disabled:bg-zinc-200 disabled:text-zinc-600`;
 
 const S = {
   SignupContainer,
+  BtnContainer,
+  BtnWrapper,
+  BtnFill,
 };
 
 export default S;
