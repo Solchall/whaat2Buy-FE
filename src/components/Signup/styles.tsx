1cca1b797f0c044ff4c677d3f82318a279dbdfc0
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-11-05 15:36:52 +0900

    fix: stepper conditional color

--- src/components/Signup/styles.tsx
+++ src/components/Signup/styles.tsx
@@ -6,19 +6,23 @@ const BtnWrapper = `cursor-pointer px-1 inline-block py-1 w-full sm:w-fit rounde
 const BtnFill = (color: string) =>
   `text-black block font-semibold bg-${color} rounded-full px-5 py-2 border-none disabled:bg-zinc-200 disabled:text-zinc-600`;
 
-const StepperColors = {
-  active: 'pink-500',
-  inActive: 'neutral-500',
+const StepperBgColors = {
+  active: 'bg-pink-500',
+  inActive: 'bg-neutral-500',
 };
 
+const StepperTextColors = {
+  active: 'text-pink-500',
+  inActive: 'text-neutral-500',
+};
 const Progresswidth = {
-  full: '98px',
-  half: '49px',
+  full: 'w-[98px]',
+  half: 'w-[49px]',
 };
 
 const setBgColor = (currentStep: number, idx: number) => {
   const bgColor =
-    currentStep < idx ? `bg-${StepperColors['inActive']}` : `bg-${StepperColors['active']}`;
+    currentStep < idx ? `${StepperBgColors['inActive']}` : `${StepperBgColors['active']}`;
   //console.log(currentStep, number - 1, currentStep >= number - 1, color);
 
   return bgColor;
@@ -26,7 +30,7 @@ const setBgColor = (currentStep: number, idx: number) => {
 
 const setTextColor = (currentStep: number, idx: number) => {
   const textColor =
-    currentStep < idx ? `text-${StepperColors['inActive']}` : `text-${StepperColors['active']}`;
+    currentStep < idx ? `${StepperTextColors['inActive']}` : `${StepperTextColors['active']}`;
   //console.log(currentStep, idx, currentStep >= idx, textColor);
 
   return textColor;
@@ -34,11 +38,11 @@ const setTextColor = (currentStep: number, idx: number) => {
 
 const setWidth = (currentStep: number, idx: number) => {
   if (currentStep == idx) {
-    return `w-[${Progresswidth['half']}]`;
+    return `${Progresswidth['half']}`;
   } else if (currentStep < idx) {
-    return `w-[${Progresswidth['full']}]`;
+    return `${Progresswidth['full']}`;
   } else {
-    return `w-[${Progresswidth['full']}]`;
+    return `${Progresswidth['full']}`;
   }
 };
 const StepContainer = 'w-full flex flex-row items-center justify-between';
